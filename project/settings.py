1428bf5efe00f52c940092e717e3cf068eb97c96
Ziad Bari <barry450643@gmail.com>
2023-11-12 22:48:50 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -128,4 +128,3 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
